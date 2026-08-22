a6f18bb48c3b3d5e7027e97ab5a09bff78695310
RU <lwj12x12@gmail.com>
2026-08-22 15:51:08 +0800

    新增股價查詢

--- 0822/app.py
+++ 0822/app.py
@@ -5,6 +5,7 @@ home = st.Page('home.py',title='首頁')
 rate = st.Page('rate.py',title='匯率查詢')
 rss = st.Page('rss.py',title='取得RSS')
 weather = st.Page('weather.py',title='天氣資訊')
+stock = st.Page('stock.py', title='股價查詢')
 
 pg = st.navigation([home,rate,rss,weather]) #導覽
 pg.run()
